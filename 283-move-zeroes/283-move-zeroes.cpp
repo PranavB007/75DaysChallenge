5e9fac2335497a90b8f27150d0eabb03e8a96717
PranavB007 <72444999+PranavB007@users.noreply.github.com>
2022-03-25 11:55:20 +0530

    Time: 35 ms (46.85%), Space: 19.3 MB (58.10%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        if(nums.size()==1 && nums[0]==0)
-            return;
-        
-        auto it = nums.begin();
-        int n = nums.size();
-        for(auto i= 0;i<n;i++)
+        int j=0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[i] ==0)
-            {
-                nums.push_back(0);
-                nums.erase(nums.begin() +i);
-                n--;
-                i--;
-            }
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+                }
         }
-    }
+        }
 };
