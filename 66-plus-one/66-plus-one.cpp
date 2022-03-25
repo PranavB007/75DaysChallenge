bc5d10ea10727da037cf7522117e715d7bd3344d
PranavB007 <72444999+PranavB007@users.noreply.github.com>
2022-03-25 12:16:55 +0530

    Time: 8 ms (9.80%), Space: 8.9 MB (13.07%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int> &digits){
-      for(int i=digits.size()-1; i>=0; i--)
+    void recc(vector<int> & d, int pos, int carr)
+    {
+        d[pos] = d[pos] + carr;
+        carr=0;
+        if(d[pos]>9)
         {
-            if(digits[i] < 9)
-            {
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
+            d[pos]  = d[pos] -10;
+            carr =1;
         }
-        digits.insert(digits.begin(), 1);
+        if(pos-1==-1 && carr==0)
+            return;
+        else if( pos-1==-1 && carr !=0)
+        {
+            d.push_back(carr);
+            reverse(d.begin(),d.end());
+            return;
+        }
+        recc(d,pos-1,carr);
+        
+    }
+    vector<int> plusOne(vector<int>& digits) {
+        recc(digits,digits.size()-1,1);
         return digits;
     }
 };
